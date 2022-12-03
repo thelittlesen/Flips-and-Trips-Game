09552dbfae228c2728e11c6add3a859333dd9872
thelittlesen <ls1153@live.mdx.ac.uk>
2022-12-03 22:52:34 +0000

    huhuh

--- Assets/LS_Scripts/LS_Activate_Reactivate_Script.cs
+++ Assets/LS_Scripts/LS_Activate_Reactivate_Script.cs
@@ -4,26 +4,5 @@ using UnityEngine;
 
 public class LS_Activate_Reactivate_Script : MonoBehaviour
 {
-    public GameObject Disappear;
 
-    void OnTriggerEnter()
-    {
-        StartCoroutine(dis());
-    }
-    IEnumerator dis()
-    {
-        yield return new WaitForSecondsRealtime(10);
-        Disappear.SetActive(false);
-    }
- 
-    if (my platform I am using is currently inactive)
-    {
-    StartCoroutine(Appear());
-}
-IEnumerator Appear()
-{
-    yield return new WaitForSecondsRealtime(10);
-    Disappear.SetActive(false);
-}
- 
 }
